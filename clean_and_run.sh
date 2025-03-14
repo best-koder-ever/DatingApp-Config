8e7cf9e0f6dd628316f5f954efbd37e9e28ba667
best-koder-ever <dettaminnsjag@gmail.com>
2025-03-14 10:42:23 +0100

    Committing changes for UserService

--- clean_and_run.sh
+++ clean_and_run.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 
-# Stop all running containers
-for container in $(docker ps -q); do
-  docker stop $container
-done
-
-# Remove all containers
-for container in $(docker ps -a -q); do
-  docker rm $container
-done
-
-# Remove all images
-for image in $(docker images -q); do
-  docker rmi $image
-done
+# Stop and remove all containers
+docker rm -f $(docker ps -aq)
 
 # Remove all volumes
-for volume in $(docker volume ls -q); do
-  docker volume rm $volume
-done
+docker volume prune -f
 
-# Remove all networks
-for network in $(docker network ls -q); do
-  docker network rm $network
-done
+# Remove unused networks
+docker network prune -f
 
-# Build and run Docker Compose
-docker-compose up --build -d
+# Restart Docker Compose without rebuilding everything
+docker-compose up -d
